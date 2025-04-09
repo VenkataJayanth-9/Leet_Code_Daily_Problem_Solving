014c9febc446ab7a544e96ac7e5d55ce05477320
VenkataJayanth9 <143101611+VenkataJayanth-9@users.noreply.github.com>
2025-04-09 20:37:34 +0530

    Time: 22 ms (61.69%) | Memory: 17.5 MB (60.02%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> array;
-        int size = nums.size();
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < size - 3; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-            for(int j = i + 1; j < size - 2; j++) {
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int left = j + 1;
-                int right = size - 1;
-                while(left < right) {
-                    long long sum = (long long)nums[left] + nums[right] + nums[i] + nums[j];
-                    if(sum == target) {
+        vector<vector<int>> array;
+
+        for(int i = 0; i<nums.size(); i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j<nums.size(); j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int left = j+1;
+                int right = nums.size()-1;
+                
+                while(left<right){
+                    long long sum = (long)(long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum == target){
                         array.push_back({nums[i], nums[j], nums[left], nums[right]});
-                        left++;
-                        right--;
-                        while(left < right && nums[left] == nums[left - 1]) left++; // Skip duplicates for left
-                        while(left < right && nums[right] == nums[right + 1]) right--; // Skip duplicates for right
+                        while(left<right && nums[left] == nums[left+1]) ++left;
+                        while(left<right && nums[right] == nums[right-1]) --right;
+                        ++left;
+                        --right;
                     }
-                    else if (sum < target) {
-                        left++;
-                    } 
-                    else {
-                        right--;
+                    else if(sum<target){
+                        ++left;
+                    }else{
+                        --right;
                     }
                 }
             }
